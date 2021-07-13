b58fa138489d37fea29fa465055c8e9c376e468d
plibjn <55870065+plibjn@users.noreply.github.com>
2021-07-13 18:02:07 +0900

    Tutorial_TransitionFixed
    화면보기 + 콜라이더 엔터 동시에

---
+++ Assets/Scripts/Tutorial/SceneChange_Circle.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+
+public class SceneChange_Circle : MonoBehaviour
+{
+
+    public PlayMakerFSM watchManager;
+    PlayMakerFSM selfFSM;
+    public bool watchBool;
+    public bool stayBool;
+    bool eventStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventStart = false;
+        selfFSM = this.gameObject.GetComponent<PlayMakerFSM>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        watchBool = FsmVariables.GlobalVariables.GetFsmBool("b_panelWatch").Value;
+
+
+
+    }
+
+    private void LateUpdate()
+    {
+
+        if (!watchBool)
+        {
+
+            // b_changeScene = false;
+            StillMoving();
+
+
+
+
+        }
+    }
+
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "HeadCollision")
+        {
+            stayBool = true;
+            if (watchBool)
+            {
+                if(eventStart !=true)
+                { StayStill(); }
+                
+               
+
+
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "HeadCollision")
+        {
+            StillMoving();
+            
+
+        }
+    }
+
+    public void StayStill()
+    {
+
+        selfFSM.SendEvent("StayStill");
+        eventStart = true;
+
+
+
+    }
+
+    public void StillMoving()
+    {
+        selfFSM.SendEvent("StillMoving");
+        eventStart = false;
+
+
+    }
+}
