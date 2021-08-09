3e842ab302b262f9207524eefa90994a291b96e0
buff0482 <buff0482@gmail.com>
2021-08-09 11:01:07 +0900

    그림자 추가

--- Assets/TestDw/Scripts/Actor.cs
+++ Assets/TestDw/Scripts/Actor.cs
@@ -7,7 +7,7 @@ using System.Linq;
 //SimpleMove(lTrans.position);
 //speechBubble.SetText("Messagesadnasdkandkandkadnkadnkasndakn");
 
-public class Actor : MonoBehaviour
+public partial class Actor : MonoBehaviour
 {
     private const string PARAM_ANIM_IS_WALK = "isWalk";
     private const string PARAM_ANIM_SPEED = "AnimationSpeed";
@@ -23,9 +23,7 @@ public class Actor : MonoBehaviour
         spriteRenderers = gameObject.GetComponentsInChildren<SpriteRenderer>();
         animator = gameObject.GetComponent<Animator>();
         animator.SetBool(PARAM_ANIM_IS_WALK, false);
-
-        //Test
-        TestMove(Vector3.left * 3f);
+        //TestMove(Vector3.left * 3f);
     }
 
     void TestMove(Vector3 dir) 
@@ -67,30 +65,55 @@ public class Actor : MonoBehaviour
             {
                 if(dir.x != 0)
                 {
-                    _.flipX = dir.x < 0;
+                    _.flipX = dir.x > 0;
                 }
             });
         });
     }
 
-    void Update()
+    //void Update()
+    //{
+    //    transform.forward = Camera.main.transform.forward;
+    //}
+}
+
+public partial class Actor//CameraFacing
+{
+    Camera referenceCamera;
+    public enum Axis { up, down, left, right, forward, back };
+    public bool reverseFace = false;
+    public Axis axis = Axis.up;
+    void Awake()
     {
-        transform.rotation = Camera.main.transform.rotation * originalRotation;
-        //transform.forward = Camera.main.transform.forward;
+        // if no camera referenced, grab the main camera
+        if (!referenceCamera)
+            referenceCamera = Camera.main;
+    }
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        // rotates the object relative to the camera
+        Vector3 targetPos = transform.position + referenceCamera.transform.rotation * (reverseFace ? Vector3.forward : Vector3.back);
+        Vector3 targetOrientation = referenceCamera.transform.rotation * GetAxis(axis);
+        transform.LookAt(targetPos, targetOrientation);
+    }
+    public Vector3 GetAxis(Axis refAxis)
+    {
+        switch (refAxis)
+        {
+            case Axis.down:
+                return Vector3.down;
+            case Axis.forward:
+                return Vector3.forward;
+            case Axis.back:
+                return Vector3.back;
+            case Axis.left:
+                return Vector3.left;
+            case Axis.right:
+                return Vector3.right;
+        }
 
-        //if (Input.GetKeyDown(KeyCode.E))
-        //{
-        //    SimpleMove(Vector3.left * 3f);
-        //}
-        //if (Input.GetKeyDown(KeyCode.R))
-        //{
-        //    SimpleMove(Vector3.right * 3f);
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.T))
-        //{
-        //    animator.SetBool(PARAM_ANIM_IS_WALK, false);
-        //    transform.DOPause();
-        //}
+        // default is Vector3.up
+        return Vector3.up;
     }
 }
