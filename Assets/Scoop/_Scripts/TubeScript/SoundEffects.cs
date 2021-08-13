72d12727955c610c0c89db8d4aa1d045aeaf0fe1
be9904 <be9904@gmail.com>
2021-08-13 21:06:48 +0900

    added audio source for recordings

---
+++ Assets/Scoop/_Scripts/TubeScript/SoundEffects.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+    [SerializeField] AudioClip stage1FinishAudio;
+    [SerializeField] AudioClip stage2FinishAudio;
+    [SerializeField] AudioClip stage3FinishAudio;
+
+    public void Stage1Finish()
+    {
+        GetComponent<AudioSource>().clip = stage1FinishAudio;
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void Stage2Finish()
+    {
+        GetComponent<AudioSource>().clip = stage2FinishAudio;
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void Stage3Finish()
+    {
+        GetComponent<AudioSource>().clip = stage3FinishAudio;
+        GetComponent<AudioSource>().Play();
+    }
+}
