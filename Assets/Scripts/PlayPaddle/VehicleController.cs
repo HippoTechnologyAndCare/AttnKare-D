6295256e32cd4e3120cfe2ca9dc8751fb86c9ec4
uk3080789 <62490021+uk3080789@users.noreply.github.com>
2021-08-20 18:15:10 +0900

    update paddle

--- Assets/Scripts/PlayPaddle/VehicleController.cs
+++ Assets/Scripts/PlayPaddle/VehicleController.cs
@@ -64,7 +64,7 @@ public class VehicleController : MonoBehaviour
 
             if (moving)
             {
-                transform.position = Vector3.MoveTowards(transform.position, EndPos, .005f);
+                transform.position = Vector3.MoveTowards(transform.position, EndPos, .008f);
             }
         }
     }
