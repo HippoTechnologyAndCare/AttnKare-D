9cccb38f5c01cb5bd656fdfc6e28592d45dcddbe
JoshRenoir <secretsigns@gmail.com>
2021-11-12 03:50:57 +0900

    adding distractions
    타겟 포인트 배열을 만들어 주기적으로 도는 기능을 추가 예정

--- Assets/Scripts/CleanUpMyRoom/NavMeshAgent/Moveable.cs
+++ Assets/Scripts/CleanUpMyRoom/NavMeshAgent/Moveable.cs
@@ -6,19 +6,44 @@ using UnityEngine.AI;
 public class Moveable : MonoBehaviour
 {
     NavMeshAgent agent;
+    Animator animator;
+    Vector3 lastPosition = Vector3.zero;
 
     [SerializeField]
     Transform target;
+    [SerializeField]
+    float speed;
+
+    public float Speed { get => speed; set => speed = value; }
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Speed = (transform.position - lastPosition).magnitude / Time.deltaTime;
+        lastPosition = transform.position;
+
+        if (Speed >= 0.05f)
+        {
+            animator.SetInteger("Status", 1);
+        }
+
+        else if (Speed < 0.05f)
+        {
+            animator.SetInteger("Status", 0);
+        }
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (!Input.GetKeyDown(KeyCode.Space))
         {
-            agent.SetDestination(target.position);
+            return;
         }
+        agent.SetDestination(target.position);
     }
 }
