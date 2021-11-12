409bc003e1ec14d872a5c6fc577b4a416aa21a4c
Josh Renoir <secretsigns@gmail.com>
2021-11-13 01:10:39 +0900

    Added ability to traverse objects
    다음 단계는 방해 오브젝트를 집었다가 놓았을때 자연스럽게 순회하는 기능을 추가 해야 함

--- Assets/Scripts/CleanUpMyRoom/NavMeshAgent/Moveable.cs
+++ Assets/Scripts/CleanUpMyRoom/NavMeshAgent/Moveable.cs
@@ -10,40 +10,89 @@ public class Moveable : MonoBehaviour
     Vector3 lastPosition = Vector3.zero;
 
     [SerializeField]
-    Transform target;
+    Transform[] targets = new Transform[4];
+
+    [SerializeField]
+    Transform parentTarget;
+
     [SerializeField]
     float speed;
+
+    [SerializeField]
+    int currentNum;
+
+    [SerializeField]
+    bool isGoing;  
 
     public float Speed { get => speed; set => speed = value; }
 
     private void Awake()
-    {
+    {        
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
     }
 
+    private void Start()
+    {        
+        for(int i = 0; i < targets.Length; i++)
+        {
+           targets[i] = parentTarget.transform.GetChild(i).transform;
+        }
+
+        isGoing = false;
+        currentNum = 0;
+        StartCoroutine(GoToTarget());
+    }
+
     void FixedUpdate()
     {
-        Speed = (transform.position - lastPosition).magnitude / Time.deltaTime;
-        lastPosition = transform.position;
-
-        if (Speed >= 0.05f)
-        {
-            animator.SetInteger("Status", 1);
-        }
-
-        else if (Speed < 0.05f)
-        {
-            animator.SetInteger("Status", 0);
-        }
+                
     }
 
     void Update()
     {
-        if (!Input.GetKeyDown(KeyCode.Space))
+        Speed = (transform.position - lastPosition).magnitude / Time.deltaTime;
+        lastPosition = transform.position;
+
+        if (Speed > 0.01f)
         {
-            return;
+            if (!isGoing)
+            {
+                isGoing = true;
+                animator.SetInteger("Status", 1);
+            }
         }
-        agent.SetDestination(target.position);
+
+        else if (Speed < 0.01f)
+        {
+            animator.SetInteger("Status", 0);
+
+            if (isGoing)
+            {
+                if (Speed == 0)
+                {
+                    isGoing = false;
+                    StartCoroutine(GoToTarget());
+                }
+            }
+        }
     }
+  
+    IEnumerator GoToTarget()
+    {       
+        agent.SetDestination(targets[currentNum].position);
+        AddCurrentNum();
+
+        yield return new WaitForEndOfFrame();        
+    }
+
+    void AddCurrentNum()
+    {        
+        currentNum++;
+
+        if (currentNum >= targets.Length)
+        {
+                currentNum = 0;
+        }          
+    }           
 }
