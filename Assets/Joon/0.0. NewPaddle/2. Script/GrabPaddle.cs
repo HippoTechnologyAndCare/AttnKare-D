692010fe6f219015854c18eb75a774fa9ca1be90
plibjn <55870065+plibjn@users.noreply.github.com>
2022-02-18 15:18:24 +0900

    midSave
    - commit before changing branch
    -need to add IDLE DISTURB

---
+++ Assets/Joon/0.0. NewPaddle/2. Script/GrabPaddle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabPaddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform MyWheel;
+    public Transform FriendWheel;
+
+    public float fFriendWheel;
+    public float fMyWheel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
