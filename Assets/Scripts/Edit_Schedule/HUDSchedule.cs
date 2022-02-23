3322ba867cf034f0e4b6a0ab92d3e2fc0d273bdd
Josh Renoir <secretsigns@gmail.com>
2022-02-24 05:36:36 +0900

    Add Dictionary Scriptable Object

--- Assets/Scripts/Edit_Schedule/HUDSchedule.cs
+++ Assets/Scripts/Edit_Schedule/HUDSchedule.cs
@@ -1,36 +1,80 @@
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
-using HutongGames.PlayMaker.Actions;
+using UnityEngine.EventSystems;
 
 [System.Serializable]
-public class SetTxtTiming
+public class TxtNTimeScript : ISerializationCallbackReceiver
 {
-    [SerializeField]
-    private List<float> keys = new List<float>();
-    [SerializeField]
-    private List<string> values = new List<string>();
+    [SerializeField] 
+    private DictionaryScriptableObject dictionaryData;
 
-    public Dictionary<float, string> TxtDictionary = new Dictionary<float, string>();
+    [SerializeField]
+    private List<string> keys = new List<string>();
+    [SerializeField]
+    private List<float> values = new List<float>();
+
+    public Dictionary<string, float> TxtDictionary = new Dictionary<string, float>();
+
+    public bool modifyValues;
+    
+    public void OnBeforeSerialize()
+    {
+        if (modifyValues) return;
+        keys.Clear();
+        values.Clear();
+    
+        for (var i = 0; i != Math.Min(dictionaryData.Keys.Count, dictionaryData.Values.Count); i++)
+        {
+            keys.Add((dictionaryData.Keys[i]));
+            values.Add(dictionaryData.Values[i]);
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        
+    }
+    
+    public void DeserializeDictionary()
+    {
+        Debug.Log("Deserialization");
+        TxtDictionary = new Dictionary<string, float>();
+        dictionaryData.Keys.Clear();
+        dictionaryData.Values.Clear();
+
+        for (var i = 0; i < Math.Min(keys.Count, values.Count); i++)
+        {
+            dictionaryData.Keys.Add(keys[i]);
+            dictionaryData.Values.Add(values[i]);
+            TxtDictionary.Add(keys[i], values[i]);
+        }
+
+        modifyValues = false;
+    }
 }
 
 public class HUDSchedule : MonoBehaviour
 {
     private enum Voice {HowTo, Start, HalfInfo, WellDone}
 
-    public SetTxtTiming setTxtTiming;
+    public TxtNTimeScript dicScript;
+    [SerializeField] DictionaryScriptableObject dicData;
 
     /*************************************************************************
     //처음 안내문구 음성과 문구을 전시합니다
     *************************************************************************/
     [SerializeField] private AudioSource audSIntro; //안내 오디오 소스
     [SerializeField] private AudioClip[] audCIntro; // 안내 음성 클립
-    [SerializeField] public DOTweenAnimation[] dotAnim; // 텍스트 애니메이션
+    [SerializeField] private DOTweenAnimation[] dotAnim; // 텍스트 애니메이션
 
+    [SerializeField] private Dictionary<string, float> txtNTimingDic;
+    
     public Canvas infoCanvas;
 
     [SerializeField] private TextMeshProUGUI howToTMP;
@@ -38,10 +82,19 @@ public class HUDSchedule : MonoBehaviour
 
     private bool _isFade;
 
+    private void Awake()
+    {
+        txtNTimingDic = dicScript.TxtDictionary;
+        for (int i = 0; i < Mathf.Min(dicData.Keys.Count, dicData.Values.Count); i++)
+        {
+            txtNTimingDic.Add(dicData.Keys[i], dicData.Values[i]);
+        }
+    }
+
     private void Start()
     {
+        _isFade = false;
         HowToPlay();
-        setTxtTiming.TxtDictionary = new Dictionary<string, float>();
     }
     
     public void HowToPlay()
@@ -54,20 +107,42 @@ public class HUDSchedule : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
         
-        foreach (var setT in howToScriptTxt)
+        foreach (var item in dicScript.TxtDictionary)
         {
+            var index = 0;
             if (!_isFade)
             {
-                howToTMP.SetText(setT);
+                howToTMP.SetText(item.Key);
                 FadeInCanvas(infoCanvas, 1f); // Info Canvas fade In
                 _isFade = true;
+                index = dicScript.TxtDictionary.Values.ToList().IndexOf(item.Value);
+                Debug.Log(index + " 번째값");
+                Debug.Log(item.Value);
+                yield return new WaitForSeconds(item.Value);
+                continue;
             }
 
-            yield return new WaitForSeconds(2f);
-            howToTMP.SetText(setT);
+            index = dicScript.TxtDictionary.Values.ToList().IndexOf(item.Value);
+            Debug.Log(index + " 번째값");
+            Debug.Log(item.Value);
+            howToTMP.SetText(item.Key);
+            yield return new WaitForSeconds(item.Value);
         }
+        
+        // foreach (var setT in howToScriptTxt)
+        // {
+        //     if (!_isFade)
+        //     {
+        //         howToTMP.SetText(setT);
+        //         FadeInCanvas(infoCanvas, 1f); // Info Canvas fade In
+        //         _isFade = true;
+        //     }
+        //
+        //     yield return new WaitForSeconds(2f);
+        //     howToTMP.SetText(setT);
+        // }
 
-        yield return new WaitForSeconds(2f);
+        //yield return new WaitForSeconds(3f);
         
         FadeOutCanvas(infoCanvas, 1f);
     }
