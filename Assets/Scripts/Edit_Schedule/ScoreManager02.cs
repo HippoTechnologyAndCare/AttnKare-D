ead652f10db64b5fc04643ef36d3aa769a4d3873
Josh Renoir <secretsigns@gmail.com>
2022-05-05 00:19:45 +0900

    Update ScoreManager02.cs

--- Assets/Scripts/Edit_Schedule/ScoreManager02.cs
+++ Assets/Scripts/Edit_Schedule/ScoreManager02.cs
@@ -16,33 +16,42 @@ public class ScoreManager02 : MonoBehaviour
     [SerializeField] private string scoreS;
     [SerializeField] private Text scoreTxt;
 
-    [SerializeField] private string[] cardArr01;
+    [SerializeField] private string[] mornCardArr;
+    [SerializeField] private string[] optionCardArr;
     [SerializeField] private string[] cardArr02;
+    
+    //test
+    [SerializeField] private string[] tier1Arr;
+    private int tier1Ctn;
+    
 
     //cardArr
 
     private void Awake()
     {
-        cardArr01 = new[] {"B", "D"};
+        mornCardArr = new[] {"A", "C", "E"};
+        optionCardArr = new[] {"B", "D"};
         cardArr02 = new[] {"F"};
-
+        
     }
 
     public void ScorerCalculator()
     {
-        // 스케줄을 완료하면서 카드 사용 정보가 모아진 CardCtnDic 사전을 활용해 점수 계산을 해야 한다
-        foreach (var card in scManager.CardCtnDic)
+        // 스케줄을 완료하면서 카드 사용 정보가 모아진 SchedulerDict 사전을 활용해 점수 계산을 해야 한다
+        foreach (var seq in scManager.SchedulerDict)
         {
-            // if (tier1Arr.Any(tier1 => card.Key == tier1))
-            // {
-            //     tier1Ctn += card.Value;
-            // }
-            //
-            // if (tier2Arr.Any(tier2 => card.Key == tier2))
-            // {
-            //     tier2Ctn += card.Value;
-            // }
-            //
+            if (mornCardArr.Any()) ;
+            
+            if (mornCardArr.Any(morning => seq.Value == morning))
+            {
+                tier1Ctn += seq.Key;
+            }
+            
+            if (tier1Arr.Any(tier1 => seq.Value == tier1))
+            {
+                tier1Ctn += seq.Key;
+            }
+            
             // if (tier3Arr.Any(tier3 => card.Key == tier3))
             // {
             //     tier3Ctn += card.Value;
